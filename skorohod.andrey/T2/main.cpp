95df7bee0e6109f1f152a67fb6fab8ac376b943a
a-sik <andreyskor07@gmail.com>
2026-06-05 11:48:07 +0300

    Fixed out

--- skorohod.andrey/T2/main.cpp
+++ skorohod.andrey/T2/main.cpp
@@ -23,7 +23,6 @@ int main()
               std::istream_iterator<DataStruct>(), std::back_inserter(data));
   }
   std::sort(data.begin(), data.end());
-  std::cout << "DataStruct:\n";
   std::copy(std::begin(data), std::end(data),\
             std::ostream_iterator< DataStruct >(std::cout, "\n"));
   return 0;
